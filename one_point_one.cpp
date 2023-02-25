02febffee74f09d4ebfba69073e727ee927e9275
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-24 16:32:41 -0800

    somemodifications to 1.1

---
+++ one_point_one.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using std::cout;
+using std::endl;
+using std::unordered_set;
+
+bool isUnique(const std::string& str){
+    // assuming the string is an ASCII string
+    if(str.size() > 128){
+        return false;
+    }
+
+    std::unordered_set<char> chars;
+    for (char c = 0; c < str.size(); c++) {
+        int val = str[c];
+        if(chars.find(c) != chars.end()){
+            return false;
+        }
+        chars.insert(c);
+    }
+    return true;
+}
+

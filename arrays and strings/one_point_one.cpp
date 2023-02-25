0db3beac06f0d72fae57120478277c147517ae7e
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-24 16:40:52 -0800

    some modifications to 1.1

--- arrays and strings/one_point_one.cpp
+++ arrays and strings/one_point_one.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
 #include <unordered_set>
+using std::unordered_set;
 
 bool isUnique(const std::string& str){
+    // assuming the string is an ASCII string
+    if(str.size() > 128){
+        return false;
+    }
+
     std::unordered_set<char> chars;
-    for (char c : str) {
+    for (char c = 0; c < str.size(); c++) {
+        int val = str[c];
         if(chars.find(c) != chars.end()){
             return false;
         }
@@ -12,3 +19,4 @@ bool isUnique(const std::string& str){
     }
     return true;
 }
+

801b799bfa01cceab26df625c53e283e63ff8cf5
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-24 17:04:43 -0800

    i added code to that same problem, but this code is to solve it without using an unordered set map

--- arrays and strings/one_point_one.cpp
+++ arrays and strings/one_point_one.cpp
@@ -2,16 +2,15 @@
 #include <string>
 #include <unordered_set>
 using std::unordered_set;
+using std::string;
 
 bool isUnique(const std::string& str){
-    // assuming the string is an ASCII string
-    if(str.size() > 128){
+    // asumming it is an ASCII string
+    if(str.size() > 128)
         return false;
-    }
-
+    // using unordered map as a hashmap
     std::unordered_set<char> chars;
     for (char c = 0; c < str.size(); c++) {
-        int val = str[c];
         if(chars.find(c) != chars.end()){
             return false;
         }
@@ -20,3 +19,17 @@ bool isUnique(const std::string& str){
     return true;
 }
 
+// if I'm not able to use additional data structure
+
+bool isUnique(string str){
+    int checker = 0;
+    for(char c = 0; c < str.size(); c++){
+        int val = str[c];
+        if((checker & (1 << val)) > 0){
+            return false;
+        }
+        checker |= (1 << val);
+    }
+
+    return true;
+}
